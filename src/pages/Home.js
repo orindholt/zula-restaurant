af38b92268cea36c02eaffa995ddeeaa34b4d839
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-06-01 13:57:33 +0200

    done with home content

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-4xl font-bold">Home</h1>
+		<div className="home content">
+			<h3 className="text-4xl font-bold">Home</h3>
+			<h2 className="text-center ">Well com to Zula</h2>
+			<div>
+				<img
+					className="zula-icon mx-auto mt-4 font-bold"
+					src="assets/zula_icon_01.png"
+					alt=""
+				/>
+			</div>
+			<h1 className="text-center font-bold text-2xl mt-4">
+				Real Teast Real Food
+			</h1>
+			<p className="text-center mt-4 ">
+				Hos Zula kommer du til at opleve mere end blot mad.
+			</p>
 		</div>
 	);
 };
