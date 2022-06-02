dd94a4ed53674f42f1e41e61d789199ebc49970a
orindholt <U75087@rts-365.dk>
2022-06-02 13:06:45 +0200

    Replace button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,30 @@
 import Layout from "../templates/Layout";
 import MenuSlide from "../components/menu-slide/MenuSlide";
-import TableBooking from "../components/table/TableBooking";
+import GenericButton from "../components/button/GenericButton";
 const Home = () => {
 	return (
-		<Layout className="text-center">
-			<h3 className="text-4xl font-bold">Home</h3>
-			<h2 className="text-center ">Welcome to Zula</h2>
+		<Layout className="text-center items-center justify-around">
+			<h2 className="text-center text-lg mt-4 font-light">Welcome to Zula</h2>
 			<div>
 				<img
-					className="zula-icon mx-auto mt-4 font-bold"
+					className="zula-icon mx-auto font-bold"
 					src="assets/zula_icon_01.png"
 					alt=""
 				/>
 			</div>
-			<h1 className="text-center font-bold text-2xl mt-4">
-				Real Teast Real Food
-			</h1>
-			<p className="text-center mt-4 ">
-				Hos Zula kommer du til at opleve mere end blot mad.
-			</p>
-			<TableBooking />
+			<div>
+				<h1 className="text-center font-bold text-2xl mt-4">
+					Real Teast Real Food
+				</h1>
+				<p className="text-center">
+					Hos Zula kommer du til at opleve mere end blot mad.
+				</p>
+			</div>
+			<GenericButton
+				caption="Book a table"
+				anchor="/"
+				className="mx-auto mt-4"
+			/>
 			<MenuSlide />
 		</Layout>
 	);
